32d18214f46aff5e8ad1074ea7776c18373cf48d
Aaron Perry <aarongperry@gmail.com>
2021-06-26 18:22:39 -0400

    updated home grid width and added tile bg colors, added colophon

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -12,77 +12,80 @@ const Home = () => {
     <div className="home">
       <div className="home-grid">
         <div
-          className="grid-item"
+          className="grid-item tv-global"
           onClick={() => handleNavigationClick("/global")}
         >
           Global
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-history"
           onClick={() => handleNavigationClick("/history")}
         >
           History
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-adult-swim"
           onClick={() => handleNavigationClick("/adult-swim")}
         >
           Adult Swim
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-teletoon"
           onClick={() => handleNavigationClick("/teletoon")}
         >
           Teletoon
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-hgtv"
           onClick={() => handleNavigationClick("/hgtv")}
         >
           HGTV
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-w"
           onClick={() => handleNavigationClick("/w-network")}
         >
           W Network
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-treehouse"
           onClick={() => handleNavigationClick("/treehouse")}
         >
           Treehouse
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-food-network"
           onClick={() => handleNavigationClick("/food-network")}
         >
           Food Network
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-slice"
           onClick={() => handleNavigationClick("/slice")}
         >
           Slice
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-show-case"
           onClick={() => handleNavigationClick("/show-case")}
         >
           Show Case
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-national-geographic"
           onClick={() => handleNavigationClick("/national-geographic")}
         >
           National Geographic
         </div>
         <div
-          className="grid-item"
+          className="grid-item tv-ytv"
           onClick={() => handleNavigationClick("/ytv")}
         >
           YTV
         </div>
+      </div>
+      <div className="colophon">
+        <p>Work in Progress 😃</p>
       </div>
     </div>
   );
